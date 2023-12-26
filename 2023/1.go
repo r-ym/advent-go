d80a87516f3c139ee8ef7d288d5224378bebf61c
r-ym <ramanyachi@gmail.com>
2023-12-26 17:45:43 +0900

    1

--- 2023/1.go
+++ 2023/1.go
@@ -1,59 +1,81 @@
 package main
 
 import (
-	"fmt"
 	"advent-go/utils"
+	"fmt"
 	"strconv"
+	"regexp"
 )
 
-func main(){
+func main() {
 	var input, _ = util.ReadURL("https://adventofcode.com/2023/day/1/input")
-	
-	part1(input)
+
+	// part1(input)
 	part2(input)
 }
 
 func part1(input []string) {
 	running_total := 0
-	max := 0
 	for _, line := range input {
-		if line != "" {
-			i, _ := strconv.Atoi(line)
-			running_total += i
-		} else {
-			if running_total > max {
-				max = running_total
+		flag := false
+		var last int
+		for _, char := range line {
+			val, err := strconv.Atoi(string(char))
+			if err == nil {
+				if !flag {
+					running_total += val * 10
+					last = val
+					flag = true
+				} else {
+					last = val
+				}
 			}
-			running_total = 0
 		}
+		running_total += last
 	}
-	fmt.Println(max)
+	fmt.Println(running_total)
 }
 
 func part2(input []string) {
+	// Define the regular expression pattern
+	re := regexp.MustCompile(`[1-9]|(one|two|three|four|five|six|seven|eight|nine)`)
+
+	// define map for string to int conversion
+	str_to_int := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+
 	running_total := 0
-	top_three_maxes := [3]int{0, 0, 0}
 	for _, line := range input {
-		if line != "" {
-			i, _ := strconv.Atoi(line)
-			running_total += i
+
+		// Find all matches of the pattern in the input string
+		matches := re.FindAllString(line, -1)
+
+		first := matches[0]
+		val1, err1 := strconv.Atoi(first)
+		if err1 == nil {
+			running_total += val1 * 10
 		} else {
-			if running_total > top_three_maxes[0] {
-				top_three_maxes[2] = top_three_maxes[1]
-				top_three_maxes[1] = top_three_maxes[0]
-				top_three_maxes[0] = running_total
-			} else if running_total > top_three_maxes[1] {
-				top_three_maxes[2] = top_three_maxes[1]
-				top_three_maxes[1] = running_total
-			} else if running_total > top_three_maxes[2] {
-				top_three_maxes[2] = running_total
-			}
-			running_total = 0
+			running_total += str_to_int[first] * 10
 		}
+
+		last := matches[len(matches)-1]
+
+		val2, err2 := strconv.Atoi(last)
+		if err2 == nil {
+			running_total += val2
+		} else {
+			running_total += str_to_int[last]
+		}
+
 	}
-	max_sum := 0
-	for _, max := range top_three_maxes{
-		max_sum += max
-	}
-	fmt.Println(max_sum)
+	fmt.Println(running_total)
 }
